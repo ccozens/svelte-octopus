57513d9808bffbe4e8358bf76ce50824dd9c0cbc
ccozens <ccozens@gmail.com>
2023-06-28 15:55:20 +0100

    created header in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script>
-    import "../app.css";
+  import { page } from "$app/stores";
+  import "../app.css";
 </script>
+
+<header class="flex justify-between px-2">
+  <div>menu</div>
+  <div>octopus plotting</div>
+  <div>something else</div>
+</header>
 
 <slot />
 
-
 <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style>
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
